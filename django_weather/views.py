eee345c42c0e404d4ab65d3d15983ba165a07f1f
goranaviani <goranaviani@gmail.com>
2022-06-29 20:44:58 +0200

    first ticket

--- django_weather/views.py
+++ django_weather/views.py
@@ -1,9 +1,2 @@
-from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from rest_auth.registration.views import SocialLoginView
+f
 
-
-class FacebookLogin(SocialLoginView):
-    adapter_class = FacebookOAuth2Adapter
-class GoogleLogin(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
